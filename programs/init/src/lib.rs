193b29e23035464687e437cc4e3434cb14b12bce
Djole <djole.nikolic.priv@gmail.com>
2021-09-16 20:09:22 +0200

    nft structure

--- programs/init/src/lib.rs
+++ programs/init/src/lib.rs
@@ -1,4 +1,5 @@
 use anchor_lang::prelude::*;
+use borsh::{BorshDeserialize, BorshSerialize};
 
 #[program]
 pub mod init {
@@ -10,3 +11,41 @@ pub mod init {
 
 #[derive(Accounts)]
 pub struct Initialize {}
+
+#[account]
+pub struct Metadata {
+    pub key: Key,
+    pub update_authority: Pubkey,
+    pub mint: Pubkey,
+    pub data: Data,
+    // Immutable, once flipped, all sales of this metadata are considered secondary.
+    pub primary_sale_happened: bool,
+    // Whether or not the data struct is mutable, default is not
+    pub is_mutable: bool,
+    /// nonce for easy calculation of editions, if present
+    pub edition_nonce: Option<u8>,
+}
+
+#[repr(C)]
+#[derive(BorshSerialize, BorshDeserialize, PartialEq, Debug, Clone)]
+pub struct Data {
+    name: String,
+    symbol: String,
+    uri: String,
+    creators: Option<Vec<Pubkey>>,
+    update_authority: Pubkey,
+    seller_fee_basis_points: u16,
+}
+
+#[repr(C)]
+#[derive(BorshSerialize, BorshDeserialize, PartialEq, Debug, Clone, Copy)]
+pub enum Key {
+    Uninitialized,
+    EditionV1,
+    MasterEditionV1,
+    ReservationListV1,
+    MetadataV1,
+    ReservationListV2,
+    MasterEditionV2,
+    EditionMarker,
+}
